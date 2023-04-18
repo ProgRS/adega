d47d766a4c0dc714c203fe551b13da23d00db062
ProgRs <lfernandotc@hotmail.com>
2023-04-18 16:25:22 -0300

    Implementação Adega estrutura base
    Implementação Adega estrutura base

---
+++ componentes/TelaCatalogo/estilos.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from 'react-native';
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  containerScroll: {
+    
+  },
+  titulo: {
+   
+  },
+  subtitulo: {
+    
+  },
+  cardVinho: {
+    
+  },
+  vinhoImg: {
+    
+  },
+  cardBoxDescricao: {
+    flex: 1
+  },
+  cardTitulo: {
+    
+  },
+  cardDescricao: {
+   
+  },
+});
+
+export default estilos;
---
+++ componentes/TelaContato/estilos.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff'
+  },
+  titulo: {
+   
+  },
+  contatoLista: {
+    
+  },
+  contatoBox: {
+    
+  },
+  contatoTitulo: {
+   
+  },
+  contatoParagrafo: {
+    
+  },
+  
+});
+
+export default estilos;
